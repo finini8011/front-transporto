8d8f8b88211866c52aa8ab6fa16321440d6ef3ae
German Mejia <germanmejiavieco@gmail.com>
2023-06-08 12:17:36 -0500

    add cards

--- src/components/commons/Cards/CardItem.jsx
+++ src/components/commons/Cards/CardItem.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import toast, { Toaster } from "react-hot-toast";
+
 import { useNavigate } from "react-router-dom";
 import ButtonForm from "../button/ButtonForm";
 
-const CardItem = ({ text, stateCards }) => {
+const CardItem = ({ text }) => {
   const navigate = useNavigate();
 
   const showAlert = () => {
-    const transformedString = text.toString().replace(".", "_");
-    if (transformedString === "1") return navigate("/lvc/");
-    else if (stateCards.state_1)  return toast.error("la tarea #1 es necesaria.");
+    const String = text.toString();
+    if (String === "1") return navigate("/lvc/");
+    else if (String) return toast.error("la tarea #1 es necesaria.");
     else return navigate("/lvc/");
   };
 
   return (
     <div className="w-full">
-      <Toaster />
+      <Toaster position="top-right" reverseOrder={false}/>
       <ButtonForm
         text={`Hacer tarea #${text} `}
         onClick={showAlert}
-        stateCards={stateCards}
         info={text}
       />
     </div>
--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Card from "../../components/commons/Cards/Card"
+import { dataCard } from "../../constants";
 
 const Home = () => {
 
-  const data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-
   return (
-<div className="flex justify-center ">
-        <div className="grid grid-cols-6 gap-4">
-          {data.map((number, key) => (
-            <Card key={key} value={number} numberCard={key} />
-          ))}
-        </div>
+    <div className="flex justify-center ">
+      <div className="grid grid-cols-6 gap-4">
+        {dataCard.map((data, key) => (
+          <Card key={key} data={data} numberCard={key} />
+        ))}
       </div>
+    </div>
 
   );
 };

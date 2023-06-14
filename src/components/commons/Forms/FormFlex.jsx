a026e8d5d2778edf33ade2d0129bb353eb22f1a3
Daniela Rique <drique@asesoftware.com>
2023-06-14 17:07:52 -0500

    Ajustes en form

--- src/components/commons/Forms/FormFlex.jsx
+++ src/components/commons/Forms/FormFlex.jsx
@@ -1,12 +1,118 @@
 import Form from "./Form";
-const FormFlex = ({inputs, buttons, titleForm, step, nameStep, cols, onSubmit}) => {
+import {
+  faDownload,
+  faEye,
+  faSquarePlus,
+} from "@fortawesome/free-solid-svg-icons";
+const FormFlex = ({titleForm, step, nameStep, cols, onSubmit}) => {
+  const inputs = [
+    {
+      label: "CREA",
+      labelWeight: "bold",
+      name: "crea",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "DESTINATARIO",
+      labelWeight: "bold",
+      name: "destinatario",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 3,
+      end: 5,
+    },
+    {
+      label: "Fecha",
+      labelWeight: "bold",
+      name: "fecha",
+      type: "text",
+      start: 5,
+      end: 5,
+    },
+    {
+      label: "Observaciones sobre el hallazgo o la no aplicación del requisito",
+      labelWeight: "bold",
+      name: "observaciones",
+      type: "textArea",
+      start: 1,
+      end: 5,
+    },
+    {
+      type: "button",
+      text: "Descargar archivo guía acta de asignación lider del PESV",
+      icon: faDownload,
+    },
+    {
+      type: "hr",
+    },
+    {
+      label: "Carga archivo",
+      labelWeight: "bold",
+      name: "cargaArchivo",
+      type: "file",
+      placeholder: "Seleccione archivo",
+      start: 1,
+      end: 4,
+      onchange: (name, value) =>
+        console.log(
+          `Función personalizada para campo ${name} - Valor: ${value}`
+        ),
+    },
+    {
+      label: "Cambiar estado",
+      labelWeight: "bold",
+      name: "cambiarEstado",
+      type: "select",
+      placeholder: "",
+      start: 4,
+      end: 6,
+    },
+    {
+      label: "Nombre Archivo cargado",
+      type: "span",
+      placeholder: "",
+      start: 1,
+      end: 4,
+      value: "documento.pdf",
+    },
+    {
+      label: "Estado actual",
+      type: "span",
+      start: 4,
+      end: 6,
+      value: "SI",
+    },
+    {
+      type: "hr",
+    },
+  ];
+  const buttons = [
+    {
+      text: "Guardar",
+      type: "submit",
+      icon: faSquarePlus,
+    },
+    {
+      text: "Ver documento",
+      type: "button",
+      icon: faEye,
+    },
+    {
+      text: "Descargar documento",
+      type: "button",
+      icon: faDownload,
+    },
+  ];
     return (
     <>
       <section className="bg-white text-gray-800 flex flex-col gap-4">
         <div className="text-white bg-primary-600 p-3 rounded-t-md text-base">
           {step} {nameStep}
         </div>
-        <Form title={titleForm} inputs={inputs} cols={cols}  buttons={buttons} onSubmit={onSubmit}/>
+        <Form title={titleForm} inputs={inputs} cols={cols}  buttons={buttons} onSubmit={onSubmit} id={step}/>
       </section>
     </>
   );

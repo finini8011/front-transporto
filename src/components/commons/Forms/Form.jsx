89f7f6291bef0e7ad57d8b252011fcc76308d9e8
laura galviz <galvizlaura69@gmail.com>
2023-06-15 09:39:03 -0500

    clearbranch

--- src/components/commons/Forms/Form.jsx
+++ src/components/commons/Forms/Form.jsx
@@ -8,7 +8,7 @@ import ButtonForm from "../button/ButtonForm";
 const Form = ({ title, inputs, cols, buttons, onSubmit, id }) => {
   const formRef = useRef(null);
 
-  const optionsChageState = ["SI", "NO"];
+  const optionsChageState = ["Seleccionar","Cumple", "NO cumple", "Cumple parcialmente"];
 
   const handleSubmit = (event) => {
     event.preventDefault();

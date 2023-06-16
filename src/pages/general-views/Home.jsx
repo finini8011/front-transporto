c1b3f2c82434fe5f237d60a4354f87b11671d7ca
laura galviz <galvizlaura69@gmail.com>
2023-06-15 22:50:55 -0500

    home

--- src/pages/general-views/Home.jsx
+++ src/pages/general-views/Home.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import Card from "../../components/commons/Cards/Card"
 import { dataCard } from "../../constants";
-import GraficColumns from "../../../grafic/GraficColumns";
-import GraficLine from "../../../grafic/GraficLine";
+
 
 const Home = () => {
 
   return (
     <div className="justify-center ">
-{/*    <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+   <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
         {dataCard.map((data, key) => (
           <Card key={key} data={data} numberCard={key} />
         ))}
-      </div> */}
-      <GraficColumns/>
-      <div className="pb-8"></div>
-      <GraficLine/>
+      </div> 
     </div>
   );
 };

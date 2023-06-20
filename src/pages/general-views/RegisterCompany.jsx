dea12f85c02159971e8063143dd6f7f1f888799d
laura galviz <galvizlaura69@gmail.com>
2023-06-20 11:19:35 -0500

    form company

--- src/pages/general-views/RegisterCompany.jsx
+++ src/pages/general-views/RegisterCompany.jsx
@@ -1,11 +1,69 @@
-import React from "react";
+import Form from "../../components/commons/Forms/Form";
+import {
+  faDownload,
+  faEye,
+  faSquarePlus,
+} from "@fortawesome/free-solid-svg-icons";
 
-const RegisterCompany = () => {
+const RegisterCompany = ({cols, onSubmit}) => {
+
+  const inputs = [
+    {
+      label: "CREA",
+      labelWeight: "bold",
+      name: "crea",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "DESTINATARIO",
+      labelWeight: "bold",
+      name: "destinatario",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 3,
+      end: 5,
+    },
+    {
+      label: "Fecha",
+      labelWeight: "bold",
+      name: "fecha",
+      type: "text",
+      start: 5,
+      end: 5,
+    },
+    {
+      label: "Observaciones sobre el hallazgo o la no aplicación del requisito",
+      labelWeight: "bold",
+      name: "observaciones",
+      type: "textArea",
+      start: 1,
+      end: 5,
+    },
+    {
+      type: "hr",
+    }
+  ];
+  const buttons = [
+    {
+      text: "Guardar",
+      type: "submit",
+      icon: faSquarePlus,
+    }
+  ];
+
+  const nameStep ="titulo";
+  const step = "empresa";
 
   return (
-    <div>
-      <p>hi</p>
+    <section className="bg-white text-gray-800 flex flex-col gap-4">
+    <div className="text-white bg-primary-600 p-3 rounded-t-md text-base">
+      {step} {nameStep}
     </div>
+    <Form title="complemento" inputs={inputs} cols={cols}  buttons={buttons} onSubmit={onSubmit} id={step}/>
+  </section>
   );
 };
 

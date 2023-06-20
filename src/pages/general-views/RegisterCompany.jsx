ff922c09f3d0b029dbc4efa852aafeac7a340db8
laura galviz <galvizlaura69@gmail.com>
2023-06-20 11:35:13 -0500

    form company

--- src/pages/general-views/RegisterCompany.jsx
+++ src/pages/general-views/RegisterCompany.jsx
@@ -9,7 +9,7 @@ const RegisterCompany = ({cols, onSubmit}) => {
 
   const inputs = [
     {
-      label: "CREA",
+      label: "NIT",
       labelWeight: "bold",
       name: "crea",
       type: "text",
@@ -18,7 +18,7 @@ const RegisterCompany = ({cols, onSubmit}) => {
       end: 3,
     },
     {
-      label: "DESTINATARIO",
+      label: "REPRESENTANTE LEGAL",
       labelWeight: "bold",
       name: "destinatario",
       type: "text",
@@ -27,43 +27,144 @@ const RegisterCompany = ({cols, onSubmit}) => {
       end: 5,
     },
     {
-      label: "Fecha",
+      label: "Actividad Principal (CIIU)",
       labelWeight: "bold",
-      name: "fecha",
+      name: "ActividadPrincipal",
+      type: "select",
+      placeholder: "",
+      start: 4,
+      end: 6,
+    },
+    {
+      label: "Actividad Secundaria(CIIU)",
+      labelWeight: "bold",
+      name: "ActividadPrincipal",
+      type: "select",
+      placeholder: "",
+      start: 4,
+      end: 6,
+    },
+    {
+      label: "Nombre y/o razon social",
+      labelWeight: "bold",
+      name: "nombre",
       type: "text",
-      start: 5,
-      end: 5,
-    },
-    {
-      label: "Observaciones sobre el hallazgo o la no aplicación del requisito",
-      labelWeight: "bold",
-      name: "observaciones",
-      type: "textArea",
+      placeholder: "Ingrese nombre",
       start: 1,
-      end: 5,
+      end: 3,
     },
     {
-      type: "hr",
+      label: "Vehiculos propios",
+      labelWeight: "bold",
+      name: "vehiculos propios",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "Veh. contratados o administrados",
+      labelWeight: "bold",
+      name: "vehiculos contratados",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "Vehiculos propios",
+      labelWeight: "bold",
+      name: "vehiculos propios",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "Conductores propios",
+      labelWeight: "bold",
+      name: "Conductores propios",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "Conductores contratados",
+      labelWeight: "bold",
+      name: "Conductores contratados",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "Direccion",
+      labelWeight: "bold",
+      name: "Direccion",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "Telefono #1",
+      labelWeight: "bold",
+      name: "Telefono #1",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "Telefono #2",
+      labelWeight: "bold",
+      name: "Telefono #2",
+      type: "text",
+      placeholder: "Ingrese nombre",
+      start: 1,
+      end: 3,
+    },
+    {
+      label: "Departamento",
+      labelWeight: "bold",
+      name: "Departamento",
+      type: "select",
+      placeholder: "",
+      start: 4,
+      end: 6,
+    },
+    {
+      label: "Cuidad",
+      labelWeight: "bold",
+      name: "Cuidad",
+      type: "select",
+      placeholder: "",
+      start: 4,
+      end: 6,
     }
   ];
   const buttons = [
     {
-      text: "Guardar",
+      text: "Registrar",
       type: "submit",
       icon: faSquarePlus,
     }
   ];
 
-  const nameStep ="titulo";
+  const nameStep ="DIGITE LOS DATOS DE LA EMPRESA";
   const step = "empresa";
 
   return (
+    <div>
+      <h1>Nueva Empresa</h1>
     <section className="bg-white text-gray-800 flex flex-col gap-4">
     <div className="text-white bg-primary-600 p-3 rounded-t-md text-base">
-      {step} {nameStep}
+      {nameStep}
     </div>
     <Form title="complemento" inputs={inputs} cols={cols}  buttons={buttons} onSubmit={onSubmit} id={step}/>
   </section>
+  </div>
   );
 };
 

904c4d4f1f238150e9245494c6efdfff6262025a
pol <pol.armaza.002@gmail.com>
2023-06-21 23:03:20 -0500

    update menu izquierdo/derecho/header

--- src/components/layout/menu/MainAuth.jsx
+++ src/components/layout/menu/MainAuth.jsx
@@ -1,41 +1,50 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate, Outlet } from "react-router-dom";
-import { selectCurrentUser } from "../../../api/features/auth/authSlice";
+import { useNavigate, Outlet, useLocation } from "react-router-dom";
+import {
+  selectCurrentUser,
+  logOut,
+} from "../../../api/features/auth/authSlice";
 import Header from "../header/header";
 
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import logo from "/img/logo.png";
+import abc from "/img/abc.png";
+import transporto from "/img/transporto.png";
+import arrow from "/img/arrow.png";
+import notification from "/img/notification.png";
+import LateralRight from "../lateralRight";
 
 import {
   faBuilding,
-  faCalendar,
+  faCalendarDays,
   faCheckSquare,
   faCogs,
   faEye,
   faFile,
   faGrip,
-  faLocationPin,
-  faLock,
+  faHouse,
+  faArrowUpRightFromSquare,
   faUserCircle,
   faVoteYea,
-  faPenToSquare
+  faPenToSquare,
 } from "@fortawesome/free-solid-svg-icons";
 import "./MainAuth.css";
 
 const MainAuth = () => {
   const user = useSelector(selectCurrentUser);
   const navigate = useNavigate();
+  const location = useLocation();
+  const [currentPage, setCurrentPage] = useState(location.pathname);
   const [openMenu, setOpenMenu] = useState();
   const [openList, setOpenList] = React.useState(true);
-
 
   const handleClick = () => {
     setOpenList(!openList);
@@ -44,108 +53,283 @@ const MainAuth = () => {
     navigate("/");
     setTimeout(() => {
       dispatch(logOut(null));
-    }, 500)
+    }, 500);
+  };
+
+  const handleNavigate = (page) => {
+    setCurrentPage(page);
+    navigate(page);
+    // Aquí puedes realizar la navegación a la página correspondiente
+  };
+
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu);
   };
 
   useEffect(() => {
     if (!user) {
-      console.log(user)
+      console.log(user);
       navigate("/");
     }
-  }, []
-  );
+  }, []);
 
   return (
     <div className="min-h-screen flex">
-      <div className={`menu-container ${openMenu ? "menu-open" : "menu-close"}`}
-        onMouseLeave={() => setOpenMenu(!openMenu)}>
-        <div className="menu-info-container">
-          <div className="flex p-2 items-center">
-            <FontAwesomeIcon icon={faUserCircle} className="mr-3 w-7 h-7" />
-            {openMenu &&
-              <h2 className="text-xl">
-                Transporto
-              </h2>}
-          </div>
-          <div className="divider"></div>
-          <div className="flex py-4 p-2 items-center">
-            <FontAwesomeIcon icon={faUserCircle} className="mr-3 w-7 h-7" />
-            {openMenu &&
-              <span className="text-lg">
-                {user.name}
-              </span>}
-          </div>
-          <div className="divider mb-2"></div>
+      <div
+        className={`shadow-md relative menu-container ${
+          openMenu ? "menu-open" : "menu-close"
+        }`}
+        // onMouseLeave={() => setOpenMenu(!openMenu)}
+      >
+        <img
+          className={`absolute right-12 top-44 cursor-pointer z-10 ${
+            openMenu ? "block" : "hidden"
+          } `}
+          src={notification}
+          alt=""
+          // onClick={handleOpenMenu}
+        />
+        <img
+          className={`absolute -right-5 top-48 cursor-pointer z-10 ${
+            openMenu ? "rotate-0" : "rotate-180"
+          } `}
+          src={arrow}
+          alt=""
+          onClick={handleOpenMenu}
+        />
+        <div
+          className={`flex items-center justify-center flex-col gap-2  border-third border-b ${
+            openMenu ? "py-2 px-4" : "p-2"
+          }`}
+        >
+          <img src={openMenu ? logo : transporto} alt="logo" />
+          {openMenu && (
+            <p className="capitalize color-primary text-xs">
+              plan estratégico de seguridad vial
+            </p>
+          )}
+          {/* <div className="divider" /> */}
         </div>
-        <List
-          sx={{ width: '100%', maxWidth: 360, bgcolor: 'transparent' }}
-          component="nav"
-          aria-labelledby="nested-list-subheader">
-          <ListItemButton
-            onClick={() => navigate("/home")}
+        <div
+          className={` border-third border-b ${openMenu ? "py-2 px-4" : "p-2"}`}
+        >
+          <div className="flex justify-center items-center gap-2">
+            <img src={abc} alt="logo" />
+            {openMenu && (
+              <h2 className="text-base text-neutral-500">ABC Transportes</h2>
+            )}
+          </div>
+        </div>
+        <div className=" text-sm pt-6 overflow-y-auto flex-1 relative">
+          <h3 className={`${openMenu ? "px-12" : "text-center"}`}>Menu</h3>
+          <List
+            sx={{ width: "100%", maxWidth: 360, bgcolor: "transparent" }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+          >
+            <ListItemButton
+              onClick={() => handleNavigate("/home")}
+              className={`flex gap-2 ${currentPage === "/home" && "active"}`}
+              sx={{ justifyContent: "center" }}
             >
-            <FontAwesomeIcon icon={faLocationPin} className="mr-3 w-5 h-5" />
-            {openMenu && <ListItemText primary="Inicio" />}
-          </ListItemButton>
-          <ListItemButton onClick={handleClick}>
-            <FontAwesomeIcon icon={faGrip} className="mr-3 w-5 h-5" />
-            {openMenu && <ListItemText primary="Fases pve" />}
-            {!openMenu ? "" :
-              openList ? <ExpandLess /> : <ExpandMore />
-            }
-          </ListItemButton>
-          {openMenu &&
-            <Collapse in={openList} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                <ListItemButton sx={{ pl: 4 }} onClick={() => navigate("/register-company")}>
-                  <FontAwesomeIcon icon={faPenToSquare} className="mr-3 w-5 h-5" />
-                  <ListItemText primary="Registrar Empresa" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }} onClick={() => navigate("/preparacion")}>
-                  <FontAwesomeIcon icon={faBuilding} className="mr-3 w-5 h-5" />
-                  <ListItemText primary="Preparación" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }} onClick={() => navigate("/planificacion")}>
-                  <FontAwesomeIcon icon={faVoteYea} className="mr-3 w-5 h-5" />
-                  <ListItemText primary="Planificación" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }} onClick={() => navigate("/implementacion")}>
-                  <FontAwesomeIcon icon={faCogs} className="mr-3 w-5 h-5" />
-                  <ListItemText primary="Implementación" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }} onClick={() => navigate("/seguimiento")}>
-                  <FontAwesomeIcon icon={faEye} className="mr-3 w-5 h-5" />
-                  <ListItemText primary="Seguimiento" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }} onClick={() => navigate("/mejora")}>
-                  <FontAwesomeIcon icon={faCheckSquare} className="mr-3 w-5 h-5" />
-                  <ListItemText primary="Mejora" />
-                </ListItemButton>
-              </List>
-            </Collapse>}
-          <ListItemButton onClick={() => navigate("/informes")}>
-            <FontAwesomeIcon icon={faFile} className="mr-3 w-5 h-5" />
-            {openMenu && <ListItemText primary="Informes" />}
-          </ListItemButton>
-          <ListItemButton onClick={() => navigate("/calendario")}>
-            <FontAwesomeIcon icon={faCalendar} className="mr-3 w-5 h-5" />
-            {openMenu && <ListItemText primary="Calendario" />}
-          </ListItemButton>
-        </List>
-        <div className="button-logout">
-          <FontAwesomeIcon icon={faLock} className="ml-3 w-5 h-5" />
-          <button className="p-2"
-            onClick={logoutSession}>
-            {openMenu && <p>Cerrar Sesión</p>}
-          </button>
+              <FontAwesomeIcon icon={faHouse} className="w-5 h-5" />
+              {openMenu && (
+                <ListItemText
+                  primary="Inicio"
+                  primaryTypographyProps={{
+                    component: "span",
+                    sx: {
+                      fontSize: "0.9rem",
+                    },
+                  }}
+                />
+              )}
+            </ListItemButton>
+            <ListItemButton
+              onClick={handleClick}
+              className="flex gap-2"
+              sx={{ justifyContent: "center" }}
+            >
+              <FontAwesomeIcon icon={faGrip} className="w-5 h-5" />
+              {openMenu && (
+                <ListItemText
+                  primary="Fases PESV"
+                  primaryTypographyProps={{
+                    component: "span",
+                    sx: {
+                      fontSize: "0.9rem",
+                    },
+                  }}
+                />
+              )}
+              {!openMenu ? "" : openList ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            {openMenu && (
+              <Collapse in={openList} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton
+                    sx={{ pl: 4 }}
+                    className={`flex gap-2 ${
+                      currentPage === "/step" && "active"
+                    }`}
+                    onClick={() => handleNavigate("/step")}
+                  >
+                    <FontAwesomeIcon icon={faPenToSquare} className="w-5 h-5" />
+                    <ListItemText
+                      primary="Registrar Empresa"
+                      primaryTypographyProps={{
+                        component: "span",
+                        sx: {
+                          fontSize: "0.9rem",
+                        },
+                      }}
+                    />
+                  </ListItemButton>
+                  <ListItemButton
+                    sx={{ pl: 4 }}
+                    onClick={() => navigate("/preparacion")}
+                    className="flex gap-2"
+                  >
+                    <FontAwesomeIcon icon={faBuilding} className="w-5 h-5" />
+                    <ListItemText
+                      primary="Preparación"
+                      primaryTypographyProps={{
+                        component: "span",
+                        sx: {
+                          fontSize: "0.9rem",
+                        },
+                      }}
+                    />
+                  </ListItemButton>
+                  <ListItemButton
+                    sx={{ pl: 4 }}
+                    onClick={() => navigate("/planificacion")}
+                    className="flex gap-2"
+                  >
+                    <FontAwesomeIcon icon={faVoteYea} className="w-5 h-5" />
+                    <ListItemText
+                      primary="Planificación"
+                      primaryTypographyProps={{
+                        component: "span",
+                        sx: {
+                          fontSize: "0.9rem",
+                        },
+                      }}
+                    />
+                  </ListItemButton>
+                  <ListItemButton
+                    sx={{ pl: 4 }}
+                    onClick={() => navigate("/implementacion")}
+                    className="flex gap-2"
+                  >
+                    <FontAwesomeIcon icon={faCogs} className="w-5 h-5" />
+                    <ListItemText
+                      primary="Implementación"
+                      primaryTypographyProps={{
+                        component: "span",
+                        sx: {
+                          fontSize: "0.9rem",
+                        },
+                      }}
+                    />
+                  </ListItemButton>
+                  <ListItemButton
+                    sx={{ pl: 4 }}
+                    onClick={() => navigate("/seguimiento")}
+                    className="flex gap-2"
+                  >
+                    <FontAwesomeIcon icon={faEye} className="w-5 h-5" />
+                    <ListItemText
+                      primary="Seguimiento"
+                      primaryTypographyProps={{
+                        component: "span",
+                        sx: {
+                          fontSize: "0.9rem",
+                        },
+                      }}
+                    />
+                  </ListItemButton>
+                  <ListItemButton
+                    sx={{ pl: 4 }}
+                    onClick={() => navigate("/mejora")}
+                    className="flex gap-2"
+                  >
+                    <FontAwesomeIcon icon={faCheckSquare} className="w-5 h-5" />
+                    <ListItemText
+                      primary="Mejora"
+                      primaryTypographyProps={{
+                        component: "span",
+                        sx: {
+                          fontSize: "0.9rem",
+                        },
+                      }}
+                    />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+            )}
+            <ListItemButton
+              onClick={() => navigate("/informes")}
+              className="flex gap-2"
+              sx={{ justifyContent: "center" }}
+            >
+              <FontAwesomeIcon icon={faFile} className="w-5 h-5" />
+              {openMenu && (
+                <ListItemText
+                  primary="Informes"
+                  primaryTypographyProps={{
+                    component: "span",
+                    sx: {
+                      fontSize: "0.9rem",
+                    },
+                  }}
+                />
+              )}
+            </ListItemButton>
+            <ListItemButton
+              onClick={() => navigate("/calendario")}
+              className="flex gap-2"
+              sx={{ justifyContent: "center" }}
+            >
+              <FontAwesomeIcon icon={faCalendarDays} className="w-5 h-5" />
+              {openMenu && (
+                <ListItemText
+                  primary="Calendario"
+                  primaryTypographyProps={{
+                    component: "span",
+                    sx: {
+                      fontSize: "0.9rem",
+                    },
+                  }}
+                />
+              )}
+            </ListItemButton>
+          </List>
+          <div className={`${openMenu ? "px-4" : "px-2"} text-white `}>
+            <div
+              className={`bg-fourth rounded-lg flex gap-2 py-2 justify-center overflow-hidden ${
+                openMenu ? "px-4" : ""
+              }`}
+              onClick={logoutSession}
+              role="button"
+            >
+              <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                className=" w-5 h-5"
+              />
+
+              {openMenu && <p>Cerrar Sesión</p>}
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex-1 h-screen bg-slate-200 flex flex-col overflow-auto">
-        <Header
-          openMenu={openMenu}
-          setOpenMenu={setOpenMenu} />
-        <div className="m-6">
-          <Outlet/>
+        <Header openMenu={openMenu} setOpenMenu={setOpenMenu} />
+        <div className="flex">
+          <div className="m-6">
+            <Outlet />
+          </div>
+          <LateralRight />
         </div>
       </div>
     </div>
--- src/pages/steps/Step3.jsx
+++ src/pages/steps/Step3.jsx
@@ -1,6 +1,6 @@
 import FormDocumentPlus from "../../components/commons/Forms/FormDocumentPlus";
 import FormFlex from "../../components/commons/Forms/FormFlex";
-import MultiSelectForm from "../../components/commons/Forms/MultiselectForm";
+// import MultiSelectForm from "../../components/commons/Forms/MultiSelectForm";
 
 const Step3 = () => {
 
@@ -12,7 +12,6 @@ const Step3 = () => {
     "¿Se le tiene designada una persona con poder de decisión en los temas relacionados con la gestión de las seguridad vial para que lidere el diseño e implementación del PESV y lo articule con el SG-SST?";
   const handleFormSubmit = (values, id) => {
     console.log(`Valores del formulario: ${id}`, values);
-
   };
   
   return (
@@ -25,13 +24,13 @@ const Step3 = () => {
         onSubmit={handleFormSubmit}
       />
       <div className="pb-10"></div>
-      <MultiSelectForm
+{/*       <MultiSelectForm
         titleForm={"titulo"}
         step={1.2}
         nameStep={"name"}
         cols={6}
         onSubmit={handleFormSubmit}
-      />
+      /> */}
       <div className="pb-10"></div>
       <FormDocumentPlus
         titleForm={"titulo2"}

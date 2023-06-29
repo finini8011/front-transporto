a42ada9b15fa0ae229b6f94b84ef9fed6db8ac07
laura galviz <galvizlaura69@gmail.com>
2023-06-28 23:16:12 -0500

    clear

--- src/components/tables/TableCheckList.jsx
+++ src/components/tables/TableCheckList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { lvc } from "../../constants/listaVerificacion";
 import { useGetStatePESVQuery } from "../../api/services/states/statesApiSlice";
-import { array } from "prop-types";
 
 
 

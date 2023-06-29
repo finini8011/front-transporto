bf283e398c7973064c9397f242f5b4037130a009
laura galviz <galvizlaura69@gmail.com>
2023-06-28 22:21:02 -0500

    Lista Verificacion con tabla

---
+++ src/components/tables/TableListVerification.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { lvc } from "../../constants/listaVerificacion";
+import { useGetStatePESVQuery } from "../../api/services/states/statesApiSlice";
+
+
+
+const TableListVerification = () => {
+
+  const { data: dataState } = useGetStatePESVQuery();
+
+
+
+  return (
+    <div className="mt-10">
+      <table className="border text-left text-sm shadow-md bg-white mb-16">
+        <thead className="">
+          <tr>
+            <th className="border p-2">#</th>
+            <th className="border p-2">Nivel PESV</th>
+            <th className="border p-2">Requisito a Verificar</th>
+            <th className="border p-2">
+              Documento sugerido para verificar según Res. 40595 de 2022
+            </th>
+            <th className="border p-2">Respuesta</th>
+            <th className="border p-2">
+              Observaciones sobre los hallazgos o la no aplicabilidad del
+              requisito
+            </th>
+          </tr>
+        </thead>
+        <tbody className="font-normal">
+          {lvc.map((data, key) => (
+            <React.Fragment key={key}>
+              <tr>
+                <td
+                  className="bg-fourth text-white text-center p-2 font-semibold"
+                  colSpan="6"
+                >
+                  {data.title}
+                </td>
+              </tr>
+              {data.body.map((content, index) => (
+                <tr className="text-start" key={index}>
+                  <td className="border p-2">{content.number} </td>
+                  <td className="border p-2">{content.level}</td>
+                  <td className="border p-2">{content.requirement}</td>
+                  <td className="border p-2">{content.document}</td>
+                  <td className="border p-2 text-center w-36">
+                    <p>prueba</p>
+                  </td>
+                  <td className="border p-2 ">
+                    <p>observaciones</p>
+                  </td>
+                </tr>
+              ))}
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableListVerification;

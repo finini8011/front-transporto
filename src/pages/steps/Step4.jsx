fa7036e721d99efd2c8a1ca6b33b8496632b5469
laura galviz <galvizlaura69@gmail.com>
2023-07-11 19:19:41 -0500

    libreria calendar

--- src/pages/steps/Step4.jsx
+++ src/pages/steps/Step4.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import pageConst from "../../../public/img/pageConst.jpg"
+import Calendar from "../../components/calendar/Calendar";
 
 const Step4 = () => {
 
 
   return (
     <div>
-      <img
+       <img
         src={pageConst}
         width={900}
         alt="icon"
         className="m-auto"
-      />
+      /> 
+      {/* <Calendar/> */}
     </div>
   );
 };

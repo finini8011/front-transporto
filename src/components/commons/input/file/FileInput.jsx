735897e1e9aabd2c59c4ec6a9e438070367bb8ba
laura galviz <galvizlaura69@gmail.com>
2023-08-16 08:03:04 -0500

    estilos

--- src/components/commons/input/file/FileInput.jsx
+++ src/components/commons/input/file/FileInput.jsx
@@ -18,8 +18,11 @@ const FileInput = ({ onFileChange }) => {
 
   return (
     <div>
-      <input type="file" accept=".csv" onChange={handleFileChange} />
-      {selectedFile && <p>Archivo seleccionado: {selectedFile.name}</p>}
+      <input 
+      className='bg-white-500 border border-[#E9EEF3] text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:outline-none focus:ring-1 focus:border-primary-600 w-full p-2.5 disabled:cursor-no-drop disabled:bg-gray-200 read-only:bg-gray-200 read-only:cursor-no-drop'
+      type="file"
+      accept=".csv" 
+      onChange={handleFileChange} />
     </div>
   );
 };

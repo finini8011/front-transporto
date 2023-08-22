9e3b2e2d19a8efdc74fd1855974f16ba29b9a497
laura galviz <galvizlaura69@gmail.com>
2023-08-22 06:46:44 -0500

    cambios

--- src/components/tables/TableStep521.jsx
+++ src/components/tables/TableStep521.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-const data = [
-  { value1: "test1", value2: "test2", value3: "test3" },
-  { value1: "test4", value2: "test5", value3: "test6" },
-  { value1: "test7", value2: "test8", value3: "test9" },
-];
 
 
-const TableStep521 = () => {
+
+const TableStep521 = ({data}) => {
   return (
     <div className="mt-10 ">
       <table className="border text-center text-sm shadow-md bg-white mb-16 w-full">
@@ -22,9 +18,9 @@ const TableStep521 = () => {
         <tbody className="font-normal">
           {data.map((data, key) => (
             <tr className="text-start" key={key}>
-              <td className="border p-2">{data.value1} </td>
-              <td className="border p-2">{data.value2}</td>
-              <td className="border p-2">{data.value3}</td>
+              <td className="border p-2">{data.valor_1} </td>
+              <td className="border p-2">{data.valor_2}</td>
+              <td className="border p-2">{data.valor_3}</td>
             </tr>
           ))}
         </tbody>

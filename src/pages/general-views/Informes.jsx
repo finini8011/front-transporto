2b9541aa28b55304eefa66ad2b40c56b8e93fa11
laura galviz <galvizlaura69@gmail.com>
2023-08-25 08:15:21 -0500

    informes

--- src/pages/general-views/Informes.jsx
+++ src/pages/general-views/Informes.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import pageConst from "../../img/pageConst.jpg"
+import SubStep511 from "../steps/substeps/subSteps5/SubStep511";
+import CardSubSteps from "../../components/commons/Cards/CardSubSteps";
 
 const Informes = () => {
+
+  const subSteps = [
+    {
+      step: '5.1.1',
+      title: 'Lista de colaboradores de la organización',
+      component: <SubStep511 />, // reemplazar por el componente que corresponda
+    },
+
+    // Agrega más subSteps si es necesario
+  ];
 
 
   return (
     <div>
-      <img
-        src={pageConst}
-        width={900}
-        alt="icon"
-        className="m-auto"
-      />
+      <CardSubSteps title={"INFORMES"} subSteps={subSteps} />
     </div>
   );
 };
